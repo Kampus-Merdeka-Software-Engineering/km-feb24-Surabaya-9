46fdeb4ba7fb1b53c89841dbcd2d9be9eddb137f
Surabaya - 9 <168883076+Surabaya-9@users.noreply.github.com>
2024-05-20 15:47:32 +0700

    Update script.js

--- UjiCoba/script.js
+++ UjiCoba/script.js
@@ -1,7 +1,27 @@
 document.getElementById('filterButton').addEventListener('click', function() {
-    var filterValue = document.getElementById('filterInput').value.toLowerCase();
+    var input = document.getElementById('filterInput').value;
+    var filterValue = input.toLowerCase();
     var table = document.getElementById('salesTable');
     var rows = table.getElementsByTagName('tr');
+    var warningMessage = document.getElementById('warningMessage');
+    var datePattern = /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/;
+
+    if (datePattern.test(input)) {
+        warningMessage.style.display = 'none';
+        for (var i = 1; i < rows.length; i++) {
+            var cell = rows[i].getElementsByTagName('td')[0];
+            if (cell) {
+                var cellValue = cell.textContent || cell.innerText;
+                if (cellValue.toLowerCase().indexOf(filterValue) > -1) {
+                    rows[i].style.display = "";
+                } else {
+                    rows[i].style.display = "none";
+                }
+            }
+        }
+    } else {
+        warningMessage.style.display = 'block';
+    }
 
     for (var i = 1; i < rows.length; i++) {
         var cell = rows[i].getElementsByTagName('td')[10]; // Kolom 11 (Sale Date)
